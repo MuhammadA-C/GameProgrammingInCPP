673a78e5a80fe994fa72e5c0d0e4e0caa5ead6ae
mchambers49 <mchambers49@gatech.edu>
2024-12-03 01:38:29 -0800

    Created Component Class
    Created Header and CPP file for Component class.

---
+++ Chapter2/Component.h
@@ -0,0 +1,28 @@
+//
+// Created by muhammad chambers on 12/3/24.
+//
+
+#ifndef CHAPTER2_COMPONENT_H
+#define CHAPTER2_COMPONENT_H
+
+
+class Component {
+public:
+    Component(class Actor* owner, int updateOrder = 100);
+    /** Destructor. */
+    virtual ~Component();
+    
+    /** Update this component by delta time. */
+    virtual void Update(float deltaTime);
+    int GetUpdateOrder() const { return mUpdateOrder; }
+
+protected:
+    /** Owning actor. */
+    class Actor* mOwner;
+    /** Update order of component. */
+    int mUpdateOrder;
+
+};
+
+
+#endif //CHAPTER2_COMPONENT_H
